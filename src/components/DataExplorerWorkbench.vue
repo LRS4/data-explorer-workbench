0e9f050a891f441bd1e6de4cfe81d4cc450bc6b9
LRS4 <42714431+LRS4@users.noreply.github.com>
2023-11-13 14:25:54 +0000

    Align disclaimers side by side

--- src/components/DataExplorerWorkbench.vue
+++ src/components/DataExplorerWorkbench.vue
@@ -2,26 +2,32 @@
   <div class="columns is-centered">
     <div class="column is-two-thirds">
       <article class="dx">
-        <h1 class="is-size-2-desktop is-size-3-mobile is-bold mb-5">
+        <h1 class="is-size-2-desktop is-size-3-mobile is-bold mb-5 has-text-left">
           Data Explorer Workbench
         </h1>
-        <b-message title="Public beta">
-            This <a href="https://en.wikipedia.org/wiki/Exploratory_data_analysis" rel="nofollow noopener noreferrer" target="_blank">EDA</a> tool is 
-            in <strong>public beta</strong> and is free to test on any 
-            <a href="https://github.com/MainakRepositor/Datasets" rel="nofollow noopener noreferrer" target="_blank">datasets</a>. 
-            When the beta closes it will require a one time purchase to access. 
-            You can find out more about the tool in 
-            the <a href="https://shedloadofcode.com/store" class="">Store</a> or watch the 
-            <a href="https://www.youtube.com/watch?v=fev2rpGxh-U" rel="nofollow noopener noreferrer" target="_blank">tutorial video</a>.
-        </b-message>
-        <b-message title="Privacy and data disclaimer" 
-          id="privacy-message-box" 
-          activearia-close-label="Close message">
-          No data from your CSV is transmitted to 
-          our server or anywhere else. All of the data stays on your computer and all of the 
-          calculations are made within your browser using JavaScript. Datasets over 20,000 rows 
-          will slow down the calculations and may cause the tool to stop working.
-        </b-message>
+        <div class="columns is-centered">
+          <div class="column is-one-half">
+            <b-message title="Public beta notice">
+                This <a href="https://en.wikipedia.org/wiki/Exploratory_data_analysis" rel="nofollow noopener noreferrer" target="_blank">EDA</a> tool is 
+                in <strong>public beta</strong> and is free to test on any 
+                <a href="https://github.com/MainakRepositor/Datasets" rel="nofollow noopener noreferrer" target="_blank">datasets</a>. 
+                When the beta closes it will require a one time purchase to access. 
+                You can find out more about the tool in 
+                the <a href="https://shedloadofcode.com/store" class="">Store</a> or watch the 
+                <a href="https://www.youtube.com/watch?v=fev2rpGxh-U" rel="nofollow noopener noreferrer" target="_blank">tutorial video</a>.
+            </b-message>
+          </div>
+          <div class="column is-one-half">
+            <b-message title="Privacy and data disclaimer" 
+              id="privacy-message-box" 
+              activearia-close-label="Close message">
+              No data from your CSV is sent to 
+              a server or anywhere else. All the data stays on your computer and the 
+              calculations are made within your browser using JavaScript. Datasets over 20,000 rows 
+              will slow down the tool considerably.
+            </b-message>
+          </div>
+        </div>
       </article>
     </div>
   </div>
